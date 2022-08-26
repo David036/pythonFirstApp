4ea0b956d0defc186ffd7613f33170933573057b
David036 <hovhannisyandav@icloud.com>
2022-08-26 16:59:38 +0400

    Fixed comments

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 professions = {
-            "profession1":"Developer",
-            "profession2":"Manager",
-            "profession3":"QA",
-            "profession4":"Marketing",
-            "profession5":"HR",
+            "DVL":"Developer",
+            "MNG":"Manager",
+            "QA":"Quality assurance",
+            "MRG":"Marketing",
+            "HR":"Human resources",
         }
 
+firstName = input("Enter your First Name:")
+lastName = input("Enter your Last Name:")
+age = input("Enter your age:")
+profession = input("MNG,DVL,QA,MRG,HR:")
 class Person:
     def __init__(self,firstName,lastName,age,profession):
-        self.firstName = firstName
+        self.firstName = firstName 
         self.lastName = lastName
         self.age = age
         self.profession = professions[profession]
 
-
-people1 = Person("Vazgen","Yan",26,"profession2")
-people2 = Person("Vazgen","YanYan",23,"profession1")
-people3 = Person("KKKKK","Yan",30,"profession1")
-people3 = Person("Vazgen","Yan",20,"profession2")
-
-peoples = [people1,people2,people3]
-
+people = Person(firstName,lastName,age,profession)
+peoples = []
+peoples.append(people)
 
 def my_function():
     filterPeoples = []
     for item in peoples:
-        if item.firstName == "Vazgen" and item.age < 25 and item.profession == "Developer":
+        if item.firstName == "Vazgen" and item.age < "25" and item.profession == "Developer":
             filterPeoples.append(item)
+        else:
+            print("Error")
 
 my_function()
